222c27fbdd2684dc1bb4b3674223b4e447d4e661
Vijay Gawate <88774056+Vijaygawate@users.noreply.github.com>
2022-03-11 15:16:12 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install fontconfig java-11-openjd
+sudo amazon-linux-extras install java-openjdk11
+yum install jenkins
